5e307994a271f2f170acb1156e4d764f950ce9d0
Sophia Guseth <sophiaguseth@gmail.com>
2022-01-19 19:45:45 -0500

    Add differential drive and controller input

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,11 +22,18 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  XboxController controller = new XboxController(0);
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+    m_exampleSubsystem.setDefaultCommand(new RunCommand(
+      () ->
+      m_exampleSubsystem.drive(-controller.getLeftY(), -controller.getRightX()),
+      m_exampleSubsystem));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -7,22 +7,31 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
 
-  TalonSRX motor1 = new TalonSRX(1);
-  TalonSRX motor2 = new TalonSRX(2);
+  DifferentialDrive drive;
+  final double LIMIT = 0.7;
 
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {
-    motor1.set(ControlMode.PercentOutput, 20);
-    motor2.set(ControlMode.PercentOutput, 20);
+    SpeedControllerSRX motor1 = new SpeedControllerSRX(1);
+    SpeedControllerSRX motor2 = new SpeedControllerSRX(2);
+    motor2.setInverted(true);
+    drive = new DifferentialDrive(motor1, motor2);
+  }
+
+  public void drive(double speed, double rotation) {
+    speed += 0.4 * Math.signum(speed);
+    speed = Math.min(speed, LIMIT);
+    speed = Math.max(speed, -LIMIT);
+    drive.arcadeDrive(speed, rotation);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 
   @Override
